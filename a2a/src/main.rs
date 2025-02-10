31623aee8648da46f0ad8c6bc280c70a5e659a37
LiJia <lijia.c@gmail.com>
2025-02-10 08:48:21 +0800

    log to file only if A2A_LOG_BASE_DIR is setting

--- a2a/src/main.rs
+++ a2a/src/main.rs
@@ -12,23 +12,27 @@ mod serve;
 
 fn setup_logging() {
   let filter = EnvFilter::from_default_env();
-  let log_base_dir = std::env::var("LOG_BASE_DIR").unwrap_or_else(|_| "./log".to_string());
-
-  std::fs::create_dir_all(&log_base_dir).unwrap_or_default();
-
-  let log_file = RollingFileAppender::builder()
-    .rotation(Rotation::DAILY)
-    .filename_prefix("a2a")
-    .filename_suffix("log")
-    .max_log_files(20)
-    .build(&log_base_dir)
-    .expect("initializing rolling file ");
+  let log_writer = std::io::stderr;
+  if let Some(log_base_dir) = std::env::var("A2A_LOG_BASE_DIR")
+    .ok()
+    .filter(|s| !s.is_empty())
+  {
+    std::fs::create_dir_all(&log_base_dir).unwrap_or_default();
+    let log_file = RollingFileAppender::builder()
+      .rotation(Rotation::DAILY)
+      .filename_prefix("a2a")
+      .filename_suffix("log")
+      .max_log_files(20)
+      .build(&log_base_dir)
+      .expect("initializing rolling file ");
+    log_writer.and(log_file);
+  }
 
   tracing_subscriber::fmt()
     .with_env_filter(filter)
     .with_ansi(false)
     .with_level(true)
-    .with_writer(std::io::stderr.and(log_file))
+    .with_writer(log_writer)
     .init();
 }
 
