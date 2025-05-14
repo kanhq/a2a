7aea77175c217411a0c6640ac0200fe6f1ca3bcd
LiJia <lijia.c@gmail.com>
2025-05-14 11:15:04 +0800

    global work dir setting

--- a2a/src/init.rs
+++ a2a/src/init.rs
@@ -1,12 +1,8 @@
-use std::{
-  io::Result,
-  io::Write,
-  path::{Path, PathBuf},
-};
+use std::{io::Result, io::Write};
 
 use tracing::info;
 
-use crate::app_conf::InitWorkDir;
+use crate::app_conf::{join_path, InitWorkDir};
 
 const ECHO_SCRIPT: &'static str = r#"
 export async function main(config, params) {
@@ -48,18 +44,10 @@ const HTML_INDEX: &'static str = r#"
 It's a2a server
 "#;
 
-fn join_path<P: AsRef<Path>>(base: P, children: &[&str]) -> PathBuf {
-  let mut p = base.as_ref().to_path_buf();
-  for child in children {
-    p.push(child);
-  }
-  p
-}
-
 pub(crate) fn init_workdir(wd: &InitWorkDir) -> Result<()> {
   let sub_dirs = ["api", "conf", "html", "scheduler"];
   for sub_dir in sub_dirs {
-    let dir = join_path(&wd.dir, &[sub_dir]);
+    let dir = join_path(&wd.root_path, &[sub_dir]);
     info!(?dir, "create sub dir");
     if !dir.exists() {
       std::fs::create_dir_all(&dir)?;
@@ -68,7 +56,7 @@ pub(crate) fn init_workdir(wd: &InitWorkDir) -> Result<()> {
 
   // create a template config file
   {
-    let config_template_path = join_path(&wd.dir, &["conf", "connection.template.yaml"]);
+    let config_template_path = join_path(&wd.root_path, &["conf", "connection.template.yaml"]);
     let conf = a2a_core::default_connection();
     let file = std::fs::File::create(&config_template_path)?;
     serde_json::to_writer_pretty(file, &conf)?;
@@ -77,7 +65,7 @@ pub(crate) fn init_workdir(wd: &InitWorkDir) -> Result<()> {
 
   // create a demo script
   {
-    let script_path = join_path(&wd.dir, &["api", "echo.js"]);
+    let script_path = join_path(&wd.root_path, &["api", "echo.js"]);
     let mut file = std::fs::File::create(&script_path)?;
     file.write_all(ECHO_SCRIPT.as_bytes())?;
     info!(file=?script_path, "create demo script");
@@ -85,7 +73,7 @@ pub(crate) fn init_workdir(wd: &InitWorkDir) -> Result<()> {
 
   // create a demo scheduler script
   {
-    let script_path = join_path(&wd.dir, &["scheduler", "echo.json"]);
+    let script_path = join_path(&wd.root_path, &["scheduler", "echo.json"]);
     let mut file = std::fs::File::create(&script_path)?;
     file.write_all(ECHO_SCHEDULER_SCRIPT.as_bytes())?;
     info!(file=?script_path, "create demo scheduler script");
@@ -93,7 +81,7 @@ pub(crate) fn init_workdir(wd: &InitWorkDir) -> Result<()> {
 
   // create a demo html file
   {
-    let html_path = join_path(&wd.dir, &["html", "index.html"]);
+    let html_path = join_path(&wd.root_path, &["html", "index.html"]);
     let mut file = std::fs::File::create(&html_path)?;
     file.write_all(HTML_INDEX.as_bytes())?;
     info!(file = ?html_path, "create demo html file");
